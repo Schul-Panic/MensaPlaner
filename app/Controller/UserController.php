0b3923860b6ae4fa81c48c531890c820d5cc50f8
Lena <FIa27.Lena.Hollstein@schueler.ovm.schule>
2026-08-27 08:59:21 +0200

    Debug Helper und ReadMe Anleitung

--- app/Controller/UserController.php
+++ app/Controller/UserController.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once __DIR__ . '/../Model/User.php';
+require_once __DIR__ . '/../Helper/Debug.php';
 
 class UserController
 {
@@ -9,6 +10,7 @@ class UserController
         $userModel = new User();
 
         $users = $userModel->getAllUsers();
+        Debug::varDumpAndDiePre($users);
 
         require __DIR__ . '/../View/users.php';
     }
---
+++ app/Helper/Debug.php
@@ -0,0 +1,20 @@
+<?php
+
+class Debug
+{
+    public static function varDumpAndDiePre(mixed $variable): void
+    {
+        echo '<pre>';
+        var_dump($variable);
+        echo '</pre>';
+        die();
+    }
+
+    public static function varDumpPre(mixed $variable): void
+    {
+        echo '<pre>';
+        var_dump($variable);
+        echo '</pre>';
+        die();
+    }
+}
